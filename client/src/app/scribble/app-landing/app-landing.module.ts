aeff369c6003bd40236404e98c0edc77e56407b4
Md Mahibul Haque <mahibulhaque2000@gmail.com>
2024-01-02 16:26:26 +0600

    chore: add major tailwind flex and grid utility classes

--- client/src/app/scribble/app-landing/app-landing.module.ts
+++ client/src/app/scribble/app-landing/app-landing.module.ts
@@ -3,20 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { RouterLinkDirective } from '../../shared-directive/router-link.directive.ts.directive';
-
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [LandingComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: LandingComponent
-      }
-    ]),
-    RouterLinkDirective
-  ]
+	declarations: [LandingComponent],
+	imports: [
+		CommonModule,
+		RouterModule.forChild([
+			{
+				path: '',
+				component: LandingComponent
+			}
+		]),
+		RouterLinkDirective,
+		MatCardModule
+	]
 })
-export class AppLandingModule { }
+export class AppLandingModule {}
